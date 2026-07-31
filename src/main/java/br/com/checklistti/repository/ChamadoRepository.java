9ce5bfcff1ea6a59430ca1a9b0798f0e4048e1c3
dinizjunioo <dinizjunior67@gmail.com>
2026-07-31 14:20:23 -0300

    algumas info sobre page e pageable

--- src/main/java/br/com/checklistti/repository/ChamadoRepository.java
+++ src/main/java/br/com/checklistti/repository/ChamadoRepository.java
@@ -9,4 +9,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ChamadoRepository extends JpaRepository<Chamado, Long> {
     Page<Chamado> findByStatus(StatusChamado status, Pageable pageable);
     Page<Chamado> findByTecnicoId(Long tecnicoId, Pageable pageable);
+
+
+    // GET /usuarios?page=0&size=10&sort=nome,asc
+    // mesma coisa
+    // Pageable pageable = PageRequest.of(0, 10, Sort.by("nome").ascending());
 }
